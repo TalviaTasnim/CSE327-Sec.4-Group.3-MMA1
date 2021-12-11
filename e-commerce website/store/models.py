07140519aff649a5de3080f75a57ff02aa08dd2f
TalviaTasnim <60611235+TalviaTasnim@users.noreply.github.com>
2021-12-11 22:39:21 +0600

    checkout page

--- e-commerce website/store/models.py
+++ e-commerce website/store/models.py
@@ -59,16 +59,7 @@ class Order(models.Model):
 		total = sum([item.quantity for item in orderitems])
 		return total 
 
-class OrderItem(models.Model):
-	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-	quantity = models.IntegerField(default=0, null=True, blank=True)
-	date_added = models.DateTimeField(auto_now_add=True)
 
-	@property
-	def get_total(self):
-		total = self.product.price * self.quantity
-		return total
 
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
