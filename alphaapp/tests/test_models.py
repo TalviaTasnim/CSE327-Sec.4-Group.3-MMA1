8d8b5213fc13494255d4608003ec409d4ea3ebe0
Md Faisal Ahmed <md.faisal3@northsouth.edu>
2022-01-03 15:11:42 +0600

    model and views test updated

--- alphaapp/tests/test_models.py
+++ alphaapp/tests/test_models.py
@@ -1,2 +1,29 @@
 from django.contrib.auth.models import User
 from alphaapp.models import User,Customer,Seller
+from django.test import client
+from django.test import TestCase, client
+
+class TestModel(TestCase):
+
+    def setUp(self):
+          self.cutomer1=Customer.objects.create(
+              first_name='mdfaisal',
+              last_name='ahmed',
+              email='mdfaisal@gmail.com' 
+          )
+
+          self.seller1=Seller.objects.create(
+              first_name='mdfaisal',
+              last_name='ahmed',
+              email='mdfaisal@gmail.com' 
+          )
+
+    def Cutomer_attribute_creation(self):
+        self.assertEquals(self.cutomer1.first_name, 'cutomer1')
+        self.assertEquals(self.cutomer1.last_name, 'cutomer1')
+        self.assertEquals(self.cutomer1.email, 'cutomer1')
+
+    def Seller_attribute_creation(self):
+        self.assertEquals(self.seller1.first_name, 'seller1')
+        self.assertEquals(self.seller1.last_name, 'seller1')
+        self.assertEquals(self.seller1.email, 'seller1')
